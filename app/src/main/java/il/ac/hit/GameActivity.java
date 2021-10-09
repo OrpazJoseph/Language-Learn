c2ba1ac5f27a796790e497bfa55495220a9a16c6
Omer <zohky0528@gmail.com>
2021-10-09 18:25:29 +0300

    getListFromDB

--- app/src/main/java/il/ac/hit/GameActivity.java
+++ app/src/main/java/il/ac/hit/GameActivity.java
@@ -6,20 +6,54 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class GameActivity extends AppCompatActivity {
-//    private TextView textView;
+    FirebaseDatabase database;
+    DatabaseReference myRef;
+    FirebaseUser mCurrentUser;
+    List<Card> mCardList;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //userName = findViewById(R.id.input);
         setContentView(R.layout.activity_game);
         setTitle(R.string.start_game);
-        //Intent intent = getIntent();
-//        String userName = intent.getStringExtra(MainActivity.INPUT_DATA_KEY);
-//        textView = findViewById(R.id.inputUserName);
-//        textView.setText(userName);
+
+        mCardList = new ArrayList<>();
+        mCurrentUser = FirebaseAuth.getInstance().getCurrentUser();
+        database = FirebaseDatabase.getInstance();
+        getListFromDB();
+    }
+
+    private void getListFromDB(){
+        myRef = database.getReference().child("users").child(mCurrentUser.getUid()).child("cards");
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                mCardList.clear();
+                for (DataSnapshot ds : snapshot.getChildren()) {
+                    mCardList.add(ds.getValue(Card.class));
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
     }
 }
