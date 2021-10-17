65a990fdfd801e4253393715ecb510b5461998f7
Omer <zohky0528@gmail.com>
2021-10-18 00:55:47 +0300

    Icon Change

--- app/src/main/java/il/ac/hit/ScoreActivity.java
+++ app/src/main/java/il/ac/hit/ScoreActivity.java
@@ -1,13 +1,13 @@
 package il.ac.hit;
 
 import android.os.Bundle;
-import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class ScoreActivity extends AppCompatActivity {
 
@@ -16,7 +16,7 @@ public class ScoreActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_score);
         setTitle(R.string.score_table);
-        ListView scoreTable = (ListView) findViewById(R.id.score_table);
+        ListView scoreTable = findViewById(R.id.score_table);
 
         ArrayList<String> arrayList = new ArrayList<>();
         arrayList.add("Moshe");
@@ -24,8 +24,13 @@ public class ScoreActivity extends AppCompatActivity {
         arrayList.add("Tomer");
 
 
-        ArrayAdapter arrayAdapter;
-        arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, arrayList);
+        ArrayAdapter<String> arrayAdapter;
+        arrayAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, arrayList);
         scoreTable.setAdapter(arrayAdapter);
+    }
+
+    private List getScoreListFromDB(){
+        // TODO: getScoreListFromDB
+        return null;
     }
 }
